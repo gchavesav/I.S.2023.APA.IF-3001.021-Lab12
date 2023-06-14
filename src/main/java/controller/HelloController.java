b72d5064f0b9093144e5f88843d385e1d029fe09
Gilberth Chaves A <gchavesav@gmail.com>
2023-06-14 17:25:09 -0600

    DirectedSinglyLinkedListGraph

--- src/main/java/controller/HelloController.java
+++ src/main/java/controller/HelloController.java
@@ -1,14 +1,71 @@
-package ucr.lab;
+package controller;
 
+import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
-import javafx.scene.control.Label;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.layout.AnchorPane;
+import javafx.scene.layout.BorderPane;
+import javafx.scene.text.Text;
+import ucr.lab.HelloApplication;
+
+import java.io.IOException;
 
 public class HelloController {
     @FXML
-    private Label welcomeText;
+    private BorderPane bp;
+    @FXML
+    private AnchorPane ap;
+    @FXML
+    private Text txtMessage;
+
+    private void loadPage(String page) {
+        FXMLLoader fxmlLoader = new FXMLLoader(HelloApplication.class.getResource(page));
+        try {
+            this.bp.setCenter(fxmlLoader.load());
+        } catch (IOException e) {
+            throw new RuntimeException(e);
+        }
+    }
 
     @FXML
-    protected void onHelloButtonClick() {
-        welcomeText.setText("Welcome to JavaFX Application!");
+    void Exit(ActionEvent event) {
+        System.exit(0);
     }
+
+    @FXML
+    void Home(ActionEvent event) {
+        this.txtMessage.setText("Laboratory No. 12");
+        this.bp.setCenter(ap);
+    }
+
+    @FXML
+    void directedGraphOnAction(ActionEvent event) {
+        loadPage("directedGraph.fxml");
+    }
+
+    @FXML
+    void directedGraphOperationsOnAction(ActionEvent event) {
+        loadPage("directedGraphOperations.fxml");
+    }
+
+    @FXML
+    void linkedGraphOnAction(ActionEvent event) {
+        loadPage("linkedGraph.fxml");
+    }
+
+    @FXML
+    void linkedGraphOperationsOnAction(ActionEvent event) {
+        loadPage("linkedGraphOperations.fxml");
+    }
+
+    @FXML
+    void mstOnAction(ActionEvent event) {
+        loadPage("mstKruskalAndPrim.fxml");
+    }
+
+    @FXML
+    void shortestPathOnAction(ActionEvent event) {
+        loadPage("dijkstra.fxml");
+    }
+
 }
