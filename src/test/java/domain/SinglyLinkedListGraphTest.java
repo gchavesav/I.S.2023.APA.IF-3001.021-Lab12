917a725f04d246edf499e8cd7a84529ece7c2336
Entiti <edward.calvoretana@ucr.ac.cr>
2023-06-14 19:20:43 -0600

    cosas de utility y test

---
+++ src/test/java/domain/SinglyLinkedListGraphTest.java
@@ -0,0 +1,47 @@
+package domain;
+
+import domain.list.ListException;
+import domain.queue.QueueException;
+import domain.stack.StackException;
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class SinglyLinkedListGraphTest {
+
+    @Test
+    void test() throws GraphException, ListException, StackException, QueueException {
+        SinglyLinkedListGraph graph = new SinglyLinkedListGraph();
+        graph.addVertex("A");
+        graph.addVertex("B");
+        graph.addVertex("C");
+        graph.addVertex("D");
+        graph.addVertex("E");
+        graph.addVertex("F");
+        graph.addVertex("G");
+        graph.addVertex("H");
+        graph.addVertex("I");
+        graph.addVertex("J");
+
+        graph.addEdgeAndWeight("A","B","Cozumel");
+        graph.addEdgeAndWeight("A","E","Ambergris Caye");
+        graph.addEdgeAndWeight("A","H","Bocas del Toro");
+        graph.addEdgeAndWeight("B","C","Útila");
+        graph.addEdgeAndWeight("C","D","Roatán");
+        graph.addEdgeAndWeight("D","H","Isla Colón");
+        graph.addEdgeAndWeight("E","F","Isla de San Martín");
+        graph.addEdgeAndWeight("E","J","Anguila");
+        graph.addEdgeAndWeight("F","J","Isla de Mona");
+        graph.addEdgeAndWeight("F","G","Isla Trinidad");
+        graph.addEdgeAndWeight("G","H","Isla La Tortuga");
+        graph.addEdgeAndWeight("B","I","Isla Tierra Bomba");
+        graph.addEdgeAndWeight("I","C","Isla de San Andrés");
+        graph.addEdgeAndWeight("I","D","Isla Popa");
+
+        System.out.println(graph.toString());
+        System.out.println(graph.dfs());
+        System.out.println(graph.bfs());
+
+
+    }
+}
