a9fbcbc9fa76790b28e9c2553421110e151149a6
DanteRunner <josuedabml@gmail.com>
2023-06-14 18:02:04 -0600

    DirectedGraph.fxml, controller sin completar

---
+++ src/main/java/controller/DirectedGraphController.java
@@ -0,0 +1,39 @@
+package controller;
+
+import javafx.event.ActionEvent;
+import javafx.scene.Group;
+import javafx.scene.control.TextArea;
+import javafx.scene.layout.Pane;
+
+public class DirectedGraphController
+{
+    @javafx.fxml.FXML
+    private TextArea textareaInfo;
+    @javafx.fxml.FXML
+    private Pane lienzo;
+    @javafx.fxml.FXML
+    private Group graf;
+
+    @javafx.fxml.FXML
+    public void initialize() {
+    }
+
+    public void ramdomizeOnAction(ActionEvent actionEvent) {
+    }
+
+    public void containsVertexOnAction(ActionEvent actionEvent) {
+    }
+
+    public void containsEdgeOnAction(ActionEvent actionEvent) {
+    }
+
+    public void toStringOnAction(ActionEvent actionEvent) {
+    }
+
+    public void bfsTourOnAction(ActionEvent actionEvent) {
+    }
+
+    public void dfsTourOnAction(ActionEvent actionEvent) {
+
+    }
+}
